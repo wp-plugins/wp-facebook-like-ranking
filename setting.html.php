a7625bb4b86e229ca2a1713a23654db69ecf4d6b
MankinJp <MankinJp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-13 09:52:40 +0000

    tiny update
    git-svn-id: https://plugins.svn.wordpress.org/wp-facebook-like-ranking/trunk@803644 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- setting.html.php
+++ setting.html.php
@@ -31,15 +31,15 @@
 				<td>
 					<select id="wp_fb_like_ranking_updatePostNumber" name="wp_fb_like_ranking_updatePostNumber">
 						<option value="all" <?php if ($WpFbLikeRankingUpdatePostNumber == 'all') echo 'selected'; ?>>全ての記事（All）</option>
-						<option value="10" <?php if ($WpFbLikeRankingUpdatePostNumber == 10) echo 'selected'; ?>>最新の10記事（10 latest）</option>
-						<option value="20" <?php if ($WpFbLikeRankingUpdatePostNumber == 20) echo 'selected'; ?>>最新の20記事（20 latest）</option>
-						<option value="30" <?php if ($WpFbLikeRankingUpdatePostNumber == 30) echo 'selected'; ?>>最新の30記事（30 latest）</option>
-						<option value="40" <?php if ($WpFbLikeRankingUpdatePostNumber == 40) echo 'selected'; ?>>最新の40記事（40 latest）</option>
-						<option value="50" <?php if ($WpFbLikeRankingUpdatePostNumber == 50) echo 'selected'; ?>>最新の50記事（50 latest）</option>
+						<option value="10" <?php if ($WpFbLikeRankingUpdatePostNumber == 10) echo 'selected'; ?>>最新の10記事（10 latest posts）</option>
+						<option value="20" <?php if ($WpFbLikeRankingUpdatePostNumber == 20) echo 'selected'; ?>>最新の20記事（20 latest posts）</option>
+						<option value="30" <?php if ($WpFbLikeRankingUpdatePostNumber == 30) echo 'selected'; ?>>最新の30記事（30 latest posts）</option>
+						<option value="40" <?php if ($WpFbLikeRankingUpdatePostNumber == 40) echo 'selected'; ?>>最新の40記事（40 latest posts）</option>
+						<option value="50" <?php if ($WpFbLikeRankingUpdatePostNumber == 50) echo 'selected'; ?>>最新の50記事（50 latest posts）</option>
 					</select>
 				</td>
 			</tr>
 		</table>
-		<input type="submit" class="button-primary" value="設定を更新(Save)" />
+		<input type="submit" class="button-primary" value="設定を更新 (Save)" />
 	</form>
 </div>
