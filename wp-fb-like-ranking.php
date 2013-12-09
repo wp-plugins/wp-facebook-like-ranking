7a7e6f1eaa40d0ebdef7e695f4a09d422c34137c
MankinJp <MankinJp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-09 07:14:02 +0000

    tiny fixing
    git-svn-id: https://plugins.svn.wordpress.org/wp-facebook-like-ranking/trunk@817257 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-fb-like-ranking.php
+++ wp-fb-like-ranking.php
@@ -1,5 +1,6 @@
 <?php
 /*
+Plugin Name: WordPress Facebook Like Ranking
 Plugin URI: http://wordpress.org/extend/plugins/wp-facebook-like-ranking/
 Description: you can use a your posts' ranking rated by the number of Facebook like./facebookのいいね数に応じた、ブログ記事のランキングを生成します。
 Version:1.13
